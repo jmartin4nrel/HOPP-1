303e5ed13293b9dba02c9e3b3c9d11426f9f0b97
jmartin4nrel <jonathan.martin@nrel.gov>
2023-01-06 18:20:13 -0700

    Can refactor wind power curve from 10 min to 1 hr averages

---
+++ examples/refactor_wind_curve.py
@@ -0,0 +1,13 @@
+import hybrid.resource.m5_wind_resource as m5
+
+start = 'August 2012'
+end = 'October 2012'
+
+# m5.import_m5_wind(start_month=start, end_month=end)
+bin_res = 4
+m5.downsample_m5_wind(bin_resolution=bin_res, start_month=start, end_month=end)
+m5.plot_m5_wind_bins(bin_resolution=bin_res, start_month=start, end_month=end)
+power_curve = "NREL_Reference_1.5MW_Turbine_Site_Level.csv"
+rotor_diameter = 77
+rho = 1.000
+m5.refactor_power_curve_m5_wind(power_curve, rotor_diameter, air_density=rho, bin_resolution=bin_res, start_month=start, end_month=end)
