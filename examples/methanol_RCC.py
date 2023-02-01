9369d2fcfd5ea128f6375f5acc0c17ec5469a0f4
jmartin4nrel <jonathan.martin@nrel.gov>
2023-01-31 17:39:56 -0700

    Harmonizing engin and finance dicts

--- examples/methanol_RCC.py
+++ examples/methanol_RCC.py
@@ -56,9 +56,8 @@ plant_lifespan = 30 # years
 discount_rate = 0.07 # (= fixed charge rate)
 TASC_multiplier = 1.093 # total overnight cost * TASC multiplier = total as-spent cost
 
-NG_price_MMBTU = 4.56 # $/MMBTU TODO: make variable NG price scenarios
-NG_LHV_MJ_kg = 47.1 # Natural gas net calorific value, MJ/kg
-H2O_price_Tgal = 2.56 # $/Tgal TODO: make variable water price scenarios
+NG_price_MMBTU = 4.56 # 2020 $/MMBTU TODO: make variable NG price scenarios
+H2O_price_Tgal = 2.56 # 2020 $/Tgal TODO: make variable water price scenarios
 
 select_locations = False # Switch to True to only analyze locations listed below
 locations = {'IA': {'on_land':[True ,], 'lat':[43.094000,], 'lon':[-93.292220,]},
@@ -72,26 +71,31 @@ manual_LCOH = False # Switch to True to override HOPP-calculated LCOE
 LCOH_kg = 0.02 # $/kg
 
 # Switch dict definitions to use certain scenarios for different technologies
-# Options for non-H2: 'Conservative', 'Moderate', 'Advanced' (based on ATB)
+# Options for non-H2, non-MeOH: 'Conservative', 'Moderate', 'Advanced' (from ATB)
 # Options for H2: 'Current', 'Future': 'Current' holds 2015 baseline H2A scenario,
 #       'Future' interpolates between that and 2040 'Future' H2A scenario
+# Options for MeOH: 'Baseline,' others to come as ASPEN models developed
+atb_scenarios = ['Advanced','Moderate','Conservative']
+H2A_scenarios = ['Current','Future']
+MeOH_scenarios = ['Baseline']
 scenarios = {'NGCC':'Conservative',
              'CCS': 'Conservative',
              'PV':  'Advanced',
-             'Wind':'Advanced',
+             'LBW': 'Advanced',
              'OSW': 'Advanced',
-             'H2':  'Future'}
+             'H2':  'Future',
+             'MeOH':'Baseline'}
 
 min_plant_dist = 120 # km, minimum distance between NGCC plants in survey
 land_rad = 60 # km radius of survey area around NGCC plant on land
 osw_rad = 20 # km radium of survey area around offshore wind location
 
-NGCC_out = 100 # MW, output of NGCC plant to scale results to
-NGCC_cap = 0.85 # capacity factor of NGCC plant to scale results to
-''' NGCC_out*NGCC_cap must be <170 MW for H2A model scaling to stay valid!
+NGCC_cap = 100 # MW, output of NGCC plant to scale results to
+NGCC_Cf = 0.85 # capacity factor of NGCC plant to scale results to
+''' NGCC_cap*NGCC_Cf must be <170 MW for H2A model scaling to stay valid!
     (H2 output needs to stay below 200,000 kg H2/day'''
 
-H2_cap = 0.97 # capacity factor of H2 plants to scale results to
+H2_Cf = 0.97 # capacity factor of H2 plants to scale results to
 
 resource_dir = Path(__file__).parent.absolute()/'resource_files'/'methanol_RCC'
 
@@ -106,6 +110,9 @@ O_MW = 15.999 # g/mol O
 L_gal = 3.78541 # L/gal
 kg_lb = 0.453592 # kg/lb
 kJ_BTU = 1.05506 # kJ/BTU
+
+NG_LHV_MJ_kg = 47.1 # Natural gas net calorific value, MJ/kg
+H2_LHV_MJ_kg = 120 # Hygrogen net calorific value, MJ/kg
 
 #endregion
 
@@ -173,88 +180,76 @@ year_rows =    {'NGCC':68,
                 'LBW': 75,
                 'OSW': 76}
 # Rows with engineering/financial inputs (MAY NEED TO CHANGE IF HOPP/SAM CHANGES)
-engin_atb ={'NGCC':{'heat_rate_BTU_Wh':{'Advanced':72,
-                                        'Moderate':73,
-                                        'Conservative':74}},
-            'CCS': {'heat_rate_BTU_Wh':{'Advanced':75,
-                                        'Moderate':76,
-                                        'Conservative':77}},
-            'PV':  {'bifaciality':     {'Advanced':75,
-                                        'Moderate':76,
-                                        'Conservative':77},
-                    'albedo':          {'Advanced':78,
-                                        'Moderate':79,
-                                        'Conservative':80},
-                    'losses':          {'Advanced':81,
-                                        'Moderate':82,
-                                        'Conservative':83},
-                    'dc_degradation':  {'Advanced':84,
-                                        'Moderate':85,
-                                        'Conservative':86}},
-            'LBW': {'hub_height':      {'Advanced':76,
-                                        'Moderate':77,
-                                        'Conservative':78},
-                    'rotor_diameter':  {'Advanced':79,
-                                        'Moderate':80,
-                                        'Conservative':81},
-                    'turbine_rating_kw':{'Advanced':82,
-                                        'Moderate':83,
-                                        'Conservative':84}},
-            'OSW': {'hub_height':      {'Advanced':76,
-                                        'Moderate':77,
-                                        'Conservative':78},
-                    'rotor_diameter':  {'Advanced':79,
-                                        'Moderate':80,
-                                        'Conservative':81},
-                    'turbine_rating_kw':{'Advanced':82,
-                                        'Moderate':83,
-                                        'Conservative':84}}}
+engin ={'NGCC':{'heat_rate_BTU_Wh':{'Advanced':72,
+                                    'Moderate':73,
+                                    'Conservative':74}},
+        'CCS': {'heat_rate_BTU_Wh':{'Advanced':75,
+                                    'Moderate':76,
+                                    'Conservative':77}},
+        'PV':  {'bifaciality':     {'Advanced':75,
+                                    'Moderate':76,
+                                    'Conservative':77},
+                'albedo':          {'Advanced':78,
+                                    'Moderate':79,
+                                    'Conservative':80},
+                'losses':          {'Advanced':81,
+                                    'Moderate':82,
+                                    'Conservative':83},
+                'dc_degradation':  {'Advanced':84,
+                                    'Moderate':85,
+                                    'Conservative':86}},
+        'LBW': {'hub_height':      {'Advanced':76,
+                                    'Moderate':77,
+                                    'Conservative':78},
+                'rotor_diameter':  {'Advanced':79,
+                                    'Moderate':80,
+                                    'Conservative':81},
+                'turbine_rating_kw':{'Advanced':82,
+                                    'Moderate':83,
+                                    'Conservative':84}},
+        'OSW': {'hub_height':      {'Advanced':76,
+                                    'Moderate':77,
+                                    'Conservative':78},
+                'rotor_diameter':  {'Advanced':79,
+                                    'Moderate':80,
+                                    'Conservative':81},
+                'turbine_rating_kw':{'Advanced':82,
+                                    'Moderate':83,
+                                    'Conservative':84}}}
 # OCC: Overnight capital cost ($/kW), FOM: Fixed O&M ($/kW-yr), VOM: Variable O&M ($/MWh)
-finance_atb =  {'NGCC':{'OCC_$_kW':    {'Advanced':105,
-                                        'Moderate':106,
-                                        'Conservative':107},
-                        'FOM_$_kWyr':  {'Advanced':116,
-                                        'Moderate':117,
-                                        'Conservative':118},
-                        'VOM_$_MWh':   {'Advanced':127,
-                                        'Moderate':128,
-                                        'Conservative':129}},
-                'CCS': {'OCC_$_kW':    {'Advanced':108,
-                                        'Moderate':109,
-                                        'Conservative':110},
-                        'FOM_$_kWyr':  {'Advanced':119,
-                                        'Moderate':120,
-                                        'Conservative':121},
-                        'VOM_$_MWh':   {'Advanced':130,
-                                        'Moderate':131,
-                                        'Conservative':132}},
-                'PV':  {'OCC_$_kW':    {'Advanced':203,
-                                        'Moderate':204,
-                                        'Conservative':205},
-                        'FOM_$_kWyr':  {'Advanced':235,
-                                        'Moderate':236,
-                                        'Conservative':237},
-                        'VOM_$_MWh':   {'Advanced':267,
-                                        'Moderate':268,
-                                        'Conservative':269}},
-                'LBW': {'OCC_$_kW':    {'Advanced':204,
-                                        'Moderate':205,
-                                        'Conservative':206},
-                        'FOM_$_kWyr':  {'Advanced':236,
-                                        'Moderate':237,
-                                        'Conservative':238},
-                        'VOM_$_MWh':   {'Advanced':268,
-                                        'Moderate':269,
-                                        'Conservative':270}},
-                'OSW': {'OCC_$_kW':    {'Advanced':253,
-                                        'Moderate':254,
-                                        'Conservative':255},
-                        'FOM_$_kWyr':  {'Advanced':297,
-                                        'Moderate':298,
-                                        'Conservative':299},
-                        'VOM_$_MWh':   {'Advanced':341,
-                                        'Moderate':342,
-                                        'Conservative':343}}}
+finance =  {'NGCC':{'OCC_$_kW':    {'Advanced':105,
+                                    'Moderate':106,
+                                    'Conservative':107}},
+            'CCS': {'OCC_$_kW':    {'Advanced':108,
+                                    'Moderate':109,
+                                    'Conservative':110}},
+            'PV':  {'OCC_$_kW':    {'Advanced':203,
+                                    'Moderate':204,
+                                    'Conservative':205},
+                    'FOM_$_kWyr':  {'Advanced':235,
+                                    'Moderate':236,
+                                    'Conservative':237},
+                    'VOM_$_MWh':   {'Advanced':267,
+                                    'Moderate':268,
+                                    'Conservative':269}},
+            'LBW': {'OCC_$_kW':    {'Advanced':204,
+                                    'Moderate':205,
+                                    'Conservative':206},
+                    'FOM_$_kWyr':  {'Advanced':236,
+                                    'Moderate':237,
+                                    'Conservative':238},
+                    'VOM_$_MWh':   {'Advanced':268,
+                                    'Moderate':269,
+                                    'Conservative':270}},
+            'OSW': {'OCC_$_kW':    {'Advanced':253,
+                                    'Moderate':254,
+                                    'Conservative':255},
+                    'FOM_$_kWyr':  {'Advanced':297,
+                                    'Moderate':298,
+                                    'Conservative':299},
+                    'VOM_$_MWh':   {'Advanced':341,
+                                    'Moderate':342,
+                                    'Conservative':343}}}
 
 # Import from spreadsheet
 workbook = load_workbook(resource_dir/filename, read_only=True, data_only=True)
@@ -272,39 +267,93 @@ for tech in atb_tech:
     start_col = get_column_letter(start_col_idx)
     end_col = get_column_letter(end_col_idx)
     # Replace row numbers in nested dicts with engin/finance params
-    for param_dict in [engin_atb, finance_atb]:
+    for param_dict in [engin, finance]:
         for param in param_dict[tech].values():
             for scenario, row in param.items():
                 cells = worksheet[start_col+str(row)+':'+end_col+str(row)][0]
                 values = []
                 for i, cell in enumerate(cells):
                     value = cell.value
-                    if param_dict is finance_atb:
+                    if param_dict is finance:
                         value = convert_dollar_year(value, basis_year, sim_basis_year)
                     values.append(value)
                 param[scenario] = values
 
 #endregion
 
+## TODO: Import natural gas price scenario
+NG_price_MMBTU = [NG_price_MMBTU]*len(sim_years)
+
+## TODO: Import natural gas price scenario
+H2O_price_Tgal = [H2O_price_Tgal]*len(sim_years)
+
 ## Import the standard NETL NGCC power plant <NETL-PUB-22638/B31A & B31B>
 #region
 
 # Values in NETL report
-fullcap_MW = {'NGCC': 727, 'CCS': 646}
-fullcap_C_kg_hr = {'NGCC': 67890, 'CCS': 61090}
-fullcap_H2O_Tgal_day = {'NGCC': 2090, 'CCS': 3437}
+NETL_basis_year = 2018
+fullcap_MW =           {'NGCC': 727, 
+                        'CCS':  646}
+fullcap_C_kg_hr =      {'NGCC': 67890, 
+                        'CCS':  61090}
+fullcap_H2O_Tgal_day = {'NGCC': 2090, 
+                        'CCS':  3437}
+FOM_kW =               {'NGCC': convert_dollar_year(26.792,NETL_basis_year,basis_year),
+                        'CCS':  convert_dollar_year(63.911,NETL_basis_year,basis_year)}
+VOM_other_MWh =        {'NGCC': convert_dollar_year(1.70517-0.22769,NETL_basis_year,basis_year),
+                        'CCS':  convert_dollar_year(5.63373-0.42133,NETL_basis_year,basis_year)}
 CO2_kg_MWh = {}
 H2O_Tgal_MWh = {}
 
 # Scale NGCC plant with CCS
-NGCC_out = {'NGCC': NGCC_out,
-            'CCS': NGCC_out*fullcap_MW['CCS']/fullcap_MW['NGCC']}
+NGCC_cap = {'NGCC': NGCC_cap,
+            'CCS': NGCC_cap*fullcap_MW['CCS']/fullcap_MW['NGCC']}
 
 # Convert to values per MWh produced
 for key, value in fullcap_C_kg_hr.items():
     CO2_kg_MWh[key] = value*(C_MW+O_MW*2)/C_MW/fullcap_MW[key]
 for key, value in fullcap_H2O_Tgal_day.items():
     H2O_Tgal_MWh[key] = value/24/fullcap_MW[key]
+
+# Add items to engin nested dict
+for plant in ['NGCC','CCS']:
+    engin[plant]['full_cap_elec_out_MWh_yr'] = fullcap_MW[plant]*8760
+    engin[plant]['elec_out_MWh_yr'] = NGCC_cap[plant]*NGCC_Cf*8760
+    engin[plant]['CO2_out_kg_MWh'] = CO2_kg_MWh[plant]
+    engin[plant]['H2O_in_Tgal_MWh'] = H2O_Tgal_MWh[plant]
+
+# Add items to finance nested dict
+VOM_comps =['VOM_H2O_$_MWh',
+            'VOM_NG_$_MWh',
+            'VOM_other_$_MWh']
+for plant in ['NGCC','CCS']:
+    finance[plant]['VOM_other_$_MWh'] = VOM_other_MWh[plant]
+    MWh_yr = engin[plant]['elec_out_MWh_yr']
+    Tgal_MWh = engin[plant]['H2O_in_Tgal_MWh']
+    finance[plant]['VOM_H2O_$_MWh'] = dict([(i,[]) for i in atb_scenarios])
+    finance[plant]['VOM_H2O_$_yr'] = dict([(i,[]) for i in atb_scenarios])
+    finance[plant]['VOM_NG_$_MWh'] = dict([(i,[]) for i in atb_scenarios])
+    finance[plant]['VOM_NG_$_yr'] = dict([(i,[]) for i in atb_scenarios])
+    for scenario in atb_scenarios:
+        for i in range(len(sim_years)):
+            price_Tgal = H2O_price_Tgal[i]
+            price_MMBTU = NG_price_MMBTU[i]
+            BTU_Wh = engin[plant]['heat_rate_BTU_Wh'][scenario][i]
+            finance[plant]['VOM_H2O_$_MWh'][scenario].append(price_Tgal*Tgal_MWh)
+            finance[plant]['VOM_H2O_$_yr'][scenario].append(price_Tgal*Tgal_MWh*MWh_yr)
+            finance[plant]['VOM_NG_$_MWh'][scenario].append(price_MMBTU*BTU_Wh)
+            finance[plant]['VOM_NG_$_yr'][scenario].append(price_MMBTU*BTU_Wh*MWh_yr)
+            VOM_MWh = 0
+            for VOM_comp in VOM_comps:
+                if type(finance[plant][VOM_comp]) is dict:
+                    if type(finance[plant][VOM_comp][scenario]) is list:
+                        VOM_MWh += finance[plant][VOM_comp][scenario][i]
+                    else:
+                        VOM_MWh += finance[plant][VOM_comp][scenario]
+                else:
+                    VOM_MWh += finance[plant][VOM_comp]
+            finance[plant]['VOM_$_MWh'] = VOM_MWh
+            finance[plant]['VOM_$_yr'] = VOM_MWh*MWh_yr
 
 #endregion
 
@@ -399,8 +448,6 @@ for id, loc in locations.items():
 
 ## TODO: Import Wind/Solar/NGCC plant LCI tables from OpenLCA
 
-## TODO: Import natural gas price scenario
-
 ## Fit curve to MeOH plant CAPEX data from IRENA report
 #region
 
@@ -462,8 +509,13 @@ CO2_emission = 0.023 # kg CO2 emitted directly per kg MeOH
 # Scale MeOH plant
 MeOH_kt_yr = {'NGCC':0, 'CCS':0}
 for ng_type in MeOH_kt_yr.keys():
-    CO2_kt_yr = CO2_kg_MWh[ng_type]*NGCC_out[ng_type]*NGCC_cap*8760/1e6
+    CO2_kt_yr = CO2_kg_MWh[ng_type]*NGCC_cap[ng_type]*NGCC_Cf*8760/1e6
     MeOH_kt_yr[ng_type] = CO2_kt_yr*mass_ratio_CO2_MeOH
+# Add items to engin and finance nested dicts
+engin['MeOH'] = {}
+finance['MeOH'] =  {'OCC_$_kW':    {'Baseline': 0},
+                    'FOM_$_kWyr':  {'Baseline': 0},
+                    'VOM_$_MWh':   {'Baseline': 0}}
 
 # Scale H2 plant
 H2_kt_yr = {'NGCC':0, 'CCS':0}
@@ -480,11 +532,12 @@ for ng_type in H2_kt_yr.keys():
 ## Import H2A model and assumptions from current and future scenarios
 #region
 
+# Get outputs from H2A model function
 for ng_type in H2_kt_yr.keys():
     for scenario in ['Current','Future']:
         H2_kg_day = H2_plant[ng_type][scenario]['H2_kg_day']
         for year in sim_years:
-            H2A_out = H2AModel_costs(H2_cap,H2_kg_day,year,scenario)
+            H2A_out = H2AModel_costs(H2_Cf,H2_kg_day,year,scenario)
             H2_basis_year, H2_capex, H2_fixed_om, kgH2O_kgH2, kWh_kgH2 = H2A_out
             H2_capex = convert_dollar_year(H2_capex, H2_basis_year, basis_year)
             H2_fixed_om = convert_dollar_year(H2_fixed_om, H2_basis_year, basis_year)
@@ -498,4 +551,18 @@ for ng_type in H2_kt_yr.keys():
             H2_plant[ng_type][scenario]['water_use_kg_kgH2'].append(kgH2O_kgH2)
             H2_plant[ng_type][scenario]['elec_use_kWh_kgH2'].append(kWh_kgH2)
 
+# Add items to engin and finance nested dicts
+engin['H2'] = {}
+finance['H2'] ={'OCC_$_kW':    {'Current':  0,
+                                'Future':   0},
+                'FOM_$_kWyr':  {'Current':  0,
+                                'Future':   0},
+                'VOM_$_MWh':   {'Current':  0,
+                                'Future':   0},}
+
+# Convert units to match other techs imported from ATB
+
 #endregion
+
+print(engin)
+print(finance)
